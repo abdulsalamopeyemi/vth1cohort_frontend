6c2e4b91d9fca08ffa66b50ee13259acebd90bb6
unknown <operaboydecentboy@gmail>
2022-11-23 12:15:52 +0100

    I learnt flex

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// javascript IS THE WORLD'S MOST POPULAR SRIPTING LANGUAGE . SINGLE LINE COMMENT
+/*
+javascript is the programming language for the web 
+javascript is easy to learn
+we will learn the following javascript concept today:
+1. Datatypes 
+2. variables
+3. operators
+
+before we begin learning let's see some of the power of javascript below
+THe original javascript version ES1 ES2 ES3 (1997-19999
+    THe first main revision ESS (2009)
+    THe second revision ES6 (2015)
+
+*/
+// function changeTodate(){
+   var curentdata= document.getElementById('role')
+   curentdata.innerhtml =data()
+
+}
+/*
+1.WE CAN USE javascript to change html content
+
+*/
+
+function changecontent(){
+    var currentvalue = document.getElementById('companyone')
+    currentvalue.innerHTML = 'mama bukka'
+}
+
+/*
+javascript can be change HTML Attribute values
+*/
+function changeAttribute(){
+    var currentATTRIBUTEValue = document.getElementById('profile_pic')
+    currentATTRIBUTEValue.src="../assets/profile_pic.png"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
